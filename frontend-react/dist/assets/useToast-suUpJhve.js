81080ae6bc88c5dd0078777fbbd801b634aaf052
Cyriac Paul <cyriac880@gmail.com>
2026-03-28 12:25:41 +0530

    first commit

--- frontend-react/dist/assets/useToast-suUpJhve.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-C6T5kWyc.js";const p=()=>{const o=t(s=>s.pushToast),a=t(s=>s.dismissToast);return{toast:(s,i="info",u=4e3)=>{o({message:s,type:i,duration:u})},dismiss:a}};export{p as u};
